fix: factor of 2 pi in mesolve hamiltonian

demos/qutip_test.py
@@ -9,14 +9,12 @@ init_ket = Qobj([[1], [0], [0], [0]]) # Bloch vector is unit x
 init_dm = init_ket * init_ket.dag()
 
 b_0 = 10
-b_1 = b_0 / 10
-gam = 1
-h_zeem = gam * b_0 * sz
-h = [h_zeem]
+b_1 = 2 * b_0 / 10 
+gam = 2
 
 larmor_freq = gam * b_0 
 period = 2 * np.pi / (larmor_freq)
-t = 1 / (2 * b_1)
+t = 1 / (2 * b_1 * gam)
 
 spin_par = {'quantum number': 3/2, 'gamma/2pi': gam}
 zeem_par = {'theta_z': 0, 'phi_z': 0, 'field magnitude': b_0}
@@ -24,10 +22,17 @@ zeem_par = {'theta_z': 0, 'phi_z': 0, 'field magnitude': b_0}
 spin, h_unpert, dm = nuclear_system_setup(spin_par=spin_par, zeem_par=zeem_par, initial_state=init_dm)
 
 
-mode = pd.DataFrame([(2 * np.pi * gam * b_0, 2 * b_1, 0., np.pi / 2, 0)], 
+mode = pd.DataFrame([(2 * np.pi * gam * b_0, b_1, 0., np.pi / 2, 0)], 
 					columns=['frequency', 'amplitude', 'phase', 'theta_p', 'phi_p'])
 
 n_points = 20
 print(np.round(evolve(spin, h_unpert, dm, solver='magnus', picture='IP', mode=mode, pulse_time=t, n_points=n_points), 3))
 print(np.round(evolve(spin, h_unpert, dm, solver='mesolve', picture='IP', mode=mode, pulse_time=t, n_points=n_points), 3))
+# dm = evolve(spin, h_unpert, dm, solver='magnus', picture='IP', mode=mode, pulse_time=t, n_points=n_points)
+# t, FID = FID_signal(spin, h_unpert, dm, acquisition_time=25, \
+# 					T2=10, reference_frequency=0, n_points=500)
+
 # low number of steps reproduces sim_vesna result
+
+# dividing frequency of pulse by 2 pi (i.e., losing leading 2 pi factor in magnus case)
+# and multiplying pulse time by t produces flip

src/pulsee/simulation.py
@@ -644,7 +644,17 @@ def evolve(spin, h_unperturbed, dm_initial, solver=mesolve, mode=None,
         return dm_evolved
 
     elif solver == mesolve or solver == 'mesolve':
-        result = mesolve(h, Qobj(dm_initial), times, options=opts)
+        scaled_h = [] 
+
+        # Magnus expansion solver includes 2 pi factor in exponentiations; 
+        # scale Hamiltonians by this factor for `mesolve` for consistency.
+        for i in h: 
+            if type(i) == list or type(i) == tuple: 
+                scaled_h.append([i[0] * 2 * np.pi, i[1]])
+            else: 
+                scaled_h.append(2 * np.pi * i)
+
+        result = mesolve(scaled_h, Qobj(dm_initial), times, options=opts)
         final_state = result.states[-1]
         return final_state # return last time step of density matrix evolution.
 
@@ -655,7 +665,6 @@ def evolve(spin, h_unperturbed, dm_initial, solver=mesolve, mode=None,
         result = solver(h, Qobj(dm_initial), times, options=opts)
         final_state = result.states[-1]
         return final_state # return last time step of density matrix evolution.
-
 
 # Operator which generates a change of picture equivalent to moving to the rotating reference frame
 # (RRF)
@@ -1047,8 +1056,11 @@ def plot_density_complex_matrix(dm, many_spin_indexing = None, show=True, phase_
 
 def FID_signal(spin, h_unperturbed, dm, acquisition_time, T2=100, theta=0, phi=0, reference_frequency=0, n_points=100):
     """ 
-    Simulates the free induction decay signal (FID) measured after the shut-off of the electromagnetic pulse, once the evolved density matrix of the system, the time interval of acquisition, the relaxation time T2 and the direction of the detection coils are given.
-  
+    Simulates the free induction decay signal (FID) measured after the shut-off
+    of the electromagnetic pulse, once the evolved density matrix of the system,
+    the time interval of acquisition, the relaxation time T2 and the direction
+    of the detection coils are given.
+    
     Parameters
     ----------
     - spin: NuclearSpin
@@ -1061,8 +1073,9 @@ def FID_signal(spin, h_unperturbed, dm, acquisition_time, T2=100, theta=0, phi=0
     
     - dm: DensityMatrix
   
-          Density matrix representing the state of the system at the beginning of the acquisition of the signal.
-    
+          Density matrix representing the state of the system at the beginning
+          of the acquisition of the signal.
+          
     - acquisition_time: float
   
                         Duration of the acquisition of the signal, expressed in microseconds.
@@ -1140,7 +1153,7 @@ def FID_signal(spin, h_unperturbed, dm, acquisition_time, T2=100, theta=0, phi=0
         # Obtain total decay envelope at that time.
         env = 1
         for dec in decay_envelopes: 
-            env *= dec(t) # Different name to avoid bizarre variable naming bug
+            env *= dec(t) # Different name to avoid bizarre variable scope bug
                           # (can't have same name as iteration var in line 1117.)
 
         dm_t = free_evolution(dm, Qobj(np.sum(h_unperturbed, axis=0)), t)

tests/test_simulation.py
@@ -107,7 +107,9 @@ def test_pi_pulse_yields_population_inversion():
     spin_par = {'quantum number' : 5/2,
                 'gamma/2pi' : 1.}
     
-    zeem_par = {'field magnitude' : 10.,
+    b0 = 10
+    b1 = 1 
+    zeem_par = {'field magnitude' : b0,
                 'theta_z' : 0,
                 'phi_z' : 0}
     
@@ -123,11 +125,11 @@ def test_pi_pulse_yields_population_inversion():
     spin, h_unperturbed, dm_initial = nuclear_system_setup(spin_par, quad_par, zeem_par, \
                                                            initial_state=initial_state)
     
-    mode = pd.DataFrame([(10., 1., 0., np.pi/2, 0)], 
+    mode = pd.DataFrame([(2 * np.pi * b0, 2 * b1, 0., np.pi/2, 0)], 
                         columns=['frequency', 'amplitude', 'phase', 'theta_p', 'phi_p'])
     
     dm_evolved = evolve(spin, h_unperturbed, dm_initial, mesolve,
-                                    mode=mode, pulse_time=2 * np.pi, picture='IP')
+                                    mode=mode, pulse_time=1 / (2 * b1), picture='IP')
 
     
     assert np.all(np.isclose(dm_evolved[5, 5], 1, rtol=1e-1))